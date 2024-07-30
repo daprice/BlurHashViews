7ec733d4fd10830679a765622ca239c125e01ee7
Dale Price <daprice@mac.com>
2024-07-30 17:36:56 -0500

    Use SwiftKMeansPlusPlus package for k-means dependency

--- Package.swift
+++ Package.swift
@@ -12,11 +12,18 @@ let package = Package(
             name: "BlurHashViews",
             targets: ["BlurHashViews"]),
     ],
+	dependencies: [
+		.package(url: "https://github.com/daprice/SwiftKMeansPlusPlus.git", from: "1.0.0"),
+	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BlurHashViews"),
+            name: "BlurHashViews",
+			dependencies: [
+				.product(name: "SwiftKMeansPlusPlus", package: "SwiftKMeansPlusPlus")
+			]
+		),
 
     ]
 )
--- Sources/BlurHashViews/Utilities/ColorArray+getPalette.swift
+++ Sources/BlurHashViews/Utilities/ColorArray+getPalette.swift
@@ -6,8 +6,8 @@
 //
 
 import SwiftUI
-
-// TODO: WIP - optionally include x and y positions within mesh as input to the k-means algorithm
+import SwiftKMeansPlusPlus
+import simd
 
 @available(iOS 17.0, *)
 @available(iOS 17, tvOS 17, visionOS 1, macOS 14, watchOS 10, macCatalyst 13, *)
@@ -25,7 +25,8 @@ extension [Color.Resolved] {
 		}
 		
 		let totalDistanceSquared = pow(maxRed - minRed, 2) + pow(maxGreen - minGreen, 2) + pow(maxBlue - minBlue, 2) + pow(maxAlpha - minAlpha, 2)
-		guard totalDistanceSquared > 0 else {
+		let totalDistance = sqrt(totalDistanceSquared)
+		guard totalDistance > 0 else {
 			// All colors are equal, return the array as-is, limited by count
 			return .init(self.prefix(resultCount))
 		}
@@ -33,12 +34,19 @@ extension [Color.Resolved] {
 		let colorVectors = map { color in
 			return SIMD4(color.linearRed, color.linearGreen, color.linearBlue, color.opacity)
 		}
-		let clusters = colorVectors.kMeansClusters(upTo: resultCount, convergeDistanceSquared: totalDistanceSquared / 100)
+		let clusters = colorVectors.kMeansClusters(upTo: resultCount, convergeDistance: totalDistance / 100)
 		
 		return clusters.sorted(by: { $0.points.count > $1.points.count }).map { cluster in
-			let closestColorToCenter = cluster.points.min(by: { distance_squared($0, cluster.center) < distance_squared($1, cluster.center) }) ?? cluster.center
+			let closestColorToCenter = cluster.points.min(by: { distanceSquared($0, cluster.center) < distanceSquared($1, cluster.center) }) ?? cluster.center
 //			let closestColorToCenter = cluster.center
 			return Color.Resolved(colorSpace: .sRGBLinear, red: closestColorToCenter[0], green: closestColorToCenter[1], blue: closestColorToCenter[2], opacity: closestColorToCenter[3])
 		}
 	}
+}
+
+/// Calculate the squared euclidean distance between any two SIMD "points".
+fileprivate func distanceSquared<P: SIMD>(_ lhs: P, _ rhs: P) -> P.Scalar where P.Scalar: FloatingPoint {
+	let diff = rhs - lhs
+	let diffSquared = diff * diff
+	return diffSquared.sum()
 }
